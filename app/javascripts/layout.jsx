c93e120ec35427ba95282ec13ab9af2e0c6e474c
Cody Poll <cjpoll@gmail.com>
2016-01-10 19:20:32 -0700

    Broke ouf main content into its own component

--- app/javascripts/layout.jsx
+++ app/javascripts/layout.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import AppHeader from 'app/javascripts/components/appheader';
+import MainContent from 'app/javascripts/components/mainContent';
+
 
 const Layout = React.createClass({
 
@@ -8,45 +10,9 @@ const Layout = React.createClass({
 			<div>
 				<AppHeader />
 
-				<div className="widewrapper main">
-					<div className="container">
-						<div className="row">
-							<div className="col-md-8 blog-main">
-								<h1> Cody Poll </h1>
-								<p> I'm a full-stack web developer, software engineer, blogger, and contractor. I write about stuff I enjoy, and all opinions are my own. </p>
-
-					<p> I've written software in lots of languages and frameworks.
-					Professionally, I've used Erlang, Elixir, Ruby, Javascript, and Java, but languages and frameworks are probably the least
-								important thing I could use to describe myself. I try to
-								write code so expressively that its intent is immediately
-								clear to the reader.  I use code that's battle tested
-								and well-maintained by instead of reinventing the wheel.
-								</p>
-
-								<p>
-								Most importantly, I think that programming is an act of
-								self-expression, drawing from creative ideas in the mind
-								to create a real-life product that solves problems. Code
-								should read like a story, and software needs to be a joy
-								to use.
-								</p>
-
-								<p> I'm currently a software engineer at Nuvi (building a Learning Management System called <a href="http://www.canvaslms.com/">Canvas</a>)</p>
-							</div>
-							<aside className="col-md-4 blog-aside">
-								<div className="aside-widget">
-									<header>
-										<h3>A simple widget</h3>
-									</header>
-									<div className="body">
-										that says hi!
-									</div>
-								</div>
-							</aside>
-						</div>
-					</div>
-				</div>
-				{this.props.children}
+				<MainContent>
+					{this.props.children}
+				</MainContent>
 				<footer>
 					<div className="widewrapper footer">
 						<div className="container">
