af047b5cef6a16e6cffe531e523d9b6e7fc21b50
Cody Poll <cjpoll@gmail.com>
2016-01-10 19:44:27 -0700

    Put side widget back in.
    There's an ugly border on the right for separating the sidebar from the
    main content. I don't like it, but I can put links to blog posts and my
    personal picture there.

--- app/javascripts/components/mainContent.jsx
+++ app/javascripts/components/mainContent.jsx
@@ -6,9 +6,19 @@ const MainContent = React.createClass({
 			<div className="widewrapper main">
 				<div className="container">
 					<div className="row">
-						<div className="col-md-12 blog-main">
+						<div className="col-md-8 blog-main">
 							{this.props.children}
 						</div>
+						<aside className="col-md-4 blog-aside">
+							<div className="aside-widget">
+								<header>
+									<h3>A simple widget</h3>
+								</header>
+								<div className="body">
+									that says hi!
+								</div>
+							</div>
+						</aside>
 					</div>
 				</div>
 			</div>
