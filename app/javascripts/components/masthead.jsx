282874def37c997eceee81634bfa9923780fef5c
Cody Poll <cjpoll@gmail.com>
2016-01-10 19:00:17 -0700

    Break out navbar into its own component

--- app/javascripts/components/masthead.jsx
+++ app/javascripts/components/masthead.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from 'app/javascripts/components/logo';
-import MobileNav from 'app/javascripts/components/mobileNav';
+import Navbar from 'app/javascripts/components/navbar';
+
 
 const Masthead = React.createClass({
 	render() {
@@ -10,31 +11,7 @@ const Masthead = React.createClass({
 				<div className="container">
 					<Logo />
 
-					<MobileNav />
-
-					<nav className="pull-right tales-nav">
-						<div className="collapse navbar-collapse">
-							<ul className="nav nav-pills navbar-nav">
-
-								<li className="dropdown active">
-									<a className="dropdown-toggle" data-toggle="dropdown" href="#">
-										Blog
-										<b className="caret"></b>
-									</a>
-									<ul className="dropdown-menu">
-										<li><a href="index.html">Blog</a></li>
-										<li><a href="blog-detail.html">Blog Detail</a></li>
-									</ul>
-								</li>
-								<li>
-									<a href="about.html">About</a>
-								</li>
-								<li>
-									<a href="credits.html">Credits</a>
-								</li>                        
-							</ul>
-						</div>
-					</nav>        
+					<Navbar />
 
 				</div>
 			</div>
